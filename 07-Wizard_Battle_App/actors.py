54f6cf2ada44c9b44e7f4bd7942ea619315ac97c
Roger Dodger <lpaddikt@gmail.com>
2018-06-29 16:45:13 -0700

    07 Wizard Battle project with Creatures, Wizard, and SmallAnimal

---
+++ 07-Wizard_Battle_App/actors.py
@@ -0,0 +1,41 @@
+import random
+
+
+class Creature:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+    def __repr__(self):
+        return f"Creature: {self.name} of level {self.level}"
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12) * self.level
+
+
+class SmallAnimal(Creature):
+    def get_defensive_roll(self):
+        base_roll = random.randint(1, 15) * self.level
+        return base_roll / 2
+
+
+class Wizard(Creature):
+
+    def attack(self, creature):
+        print(f"The wizard {self.name} attacks {creature.name}")
+
+        my_roll = random.randint(1, 12) * self.level
+        # creature_roll = random.randint(1, 12) * creature.level
+        creature_roll = self.get_defensive_roll()
+
+        print(f"You roll {my_roll}")
+        print(f"{creature.name} rolls {creature_roll}")
+
+        if my_roll >= creature_roll:
+            print(f"The wizard has handily defeated {creature.name}")
+            return True
+        else:
+            print("The wizard has been DEFEATED")
+            return False
+
+
--- 07-Wizard_Battle_App/program.py
+++ 07-Wizard_Battle_App/program.py
@@ -1,3 +1,7 @@
+import random
+import time
+
+from actors import Wizard, Creature, SmallAnimal
 
 
 def main():
@@ -12,16 +16,44 @@ def print_header():
 
 
 def game_loop():
+
+    creatures = [
+        SmallAnimal('Toad', 1),
+        Creature('Tiger', 12),
+        SmallAnimal('Bat', 3),
+        Creature('Dragon', 50),
+        Wizard('Evil Wizard', 1000)
+    ]
+
+    print(creatures)
+
+    hero = Wizard('Gandolf', 75)
+
     while True:
+
+        active_creature = random.choice(creatures)
+        print(f"A {active_creature.name} of level {active_creature.level} has appeared from a dark and foggy forest.\n")
+
         cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hids taking time to recover...")
+                time.sleep(5)
+                print("The wizard returns revitalized!!!")
         elif cmd == 'r':
-            print('runaway')
+            print('The wizard has been unsure of his powers and flees...')
         elif cmd == 'l':
-            print('look around')
+            print(f'The wizard {hero.name} takes in the surroundings and sees:')
+            for c in creatures:
+                print(f" * A {c.name} of level {c.level}")
         else:
             print('OK, exiting game...bye!!!')
+            break
+
+        if not creatures:
+            print("You've defeated all the creatures, well done!")
             break
 
 
