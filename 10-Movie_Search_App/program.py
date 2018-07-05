852b722fbc23fc27dd8ad58fb4776b0726f2d2a8
Roger Dodger <lpaddikt@gmail.com>
2018-07-05 15:48:50 -0700

    10 Added exceptions and finalized

--- 10-Movie_Search_App/program.py
+++ 10-Movie_Search_App/program.py
@@ -1,4 +1,5 @@
 import movie_svc
+import requests.exceptions
 
 
 def main():
@@ -17,13 +18,20 @@ def search_event_loop():
     search = "ONCE_THROUGH_LOOP"
 
     while search != 'x':
-        search = input('Movie search text (x to exit): ')
-        if search != 'x':
-            results = movie_svc.find_movies(search)
-            print(f'Found {len(results)}')
-            for r in results:
-                print(f'{r.year} -- {r.title}')
-            print()
+        try:
+            search = input('Movie search text (x to exit): ')
+            if search != 'x':
+                results = movie_svc.find_movies(search)
+                print(f'Found {len(results)}')
+                for r in results:
+                    print(f'{r.year} -- {r.title}')
+                print()
+        except ValueError:
+            print("Error: Search text is required")
+        except requests.exceptions.ConnectionError:
+            print("Error your network is down.")
+        except Exception as x:
+            print(f"YIKES, that didn't work! Details: {x}")
 
     print('exiting...')
 
